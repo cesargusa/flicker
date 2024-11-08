a7ac0ac275fd474d8a1b06ea94d422c749861584
cesargusa <cesargusa22@gmail.com>
2024-11-08 23:24:05 +0100

    changes vercel

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 import userRegisterRoutes from './api/routes/userRegisterRoutes.js';
 import userLoginRoutes from './api/routes/userLoginRoutes.js';
-import authMiddleware from './middleware/authMiddleware.js';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config(); // Solo cargar dotenv en desarrollo
+}
 
 const supabaseUrl = 'https://daavmqheycjwoipjmexk.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
@@ -14,7 +15,9 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const app = express();
 app.use(express.json());
 
+// Configura las rutas
 app.use('/users/users', userRegisterRoutes);
 app.use('/users/usersLogin', userLoginRoutes);
 
-export { app, supabase };
+// Exporta una función que actúe como handler para Vercel
+export default (req, res) => app(req, res);
