47365dd55514f9380dc68a5e3a5fb84b3dfaec42
cesargusa <cesargusa22@gmail.com>
2024-11-08 23:14:36 +0100

    config vercel

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// index.js
-
 import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
-import userRegisterRoutes from './api/routes/userRegisterRoutes.js'; // Asegúrate de que esta ruta sea correcta
-import userLoginRoutes from './api/routes/userLoginRoutes.js'; // Asegúrate de que esta ruta sea correcta
-import authMiddleware  from './middleware/authMiddleware.js';
+import userRegisterRoutes from './api/routes/userRegisterRoutes.js';
+import userLoginRoutes from './api/routes/userLoginRoutes.js';
+import authMiddleware from './middleware/authMiddleware.js';
+
 dotenv.config();
 
 const supabaseUrl = 'https://daavmqheycjwoipjmexk.supabase.co';
@@ -15,18 +14,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const app = express();
 app.use(express.json());
 
-// Usa el enrutador de usuarios
-app.use('/users/users', userRegisterRoutes); // Rutas de usuario estarán disponibles en '/users'
-app.use('/users/usersLogin', userLoginRoutes); // Rutas de usuario estarán disponibles en '/users'
+app.use('/users/users', userRegisterRoutes);
+app.use('/users/usersLogin', userLoginRoutes);
 
-// Iniciar el servidor
-const startServer = () => {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Servidor API escuchando en http://localhost:${PORT}`);
-    });
-};
-
-startServer();
-
-export { app, startServer, supabase };
+export { app, supabase };
