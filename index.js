bc2d114f53146c79f9ccf969592741cfaf54fa29
cesargusa <cesargusa22@gmail.com>
2025-01-05 21:34:09 +0100

    vercel change

--- index.js
+++ index.js
@@ -12,18 +12,23 @@ if (process.env.NODE_ENV !== 'production') {
 
 const supabaseUrl = 'https://vrrgusfrgazctcldrryo.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Configura las rutas
+
 app.use('/users',usersRoutes);
 app.use('/action', actionRoute);
 
+// Configura el puerto (si estás en local puedes usar un puerto como 3000)
 const PORT = process.env.PORT || 3000;  // Usa el puerto definido en .env o 3000 por defecto
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
+// Exporta una función que actúe como handler para Vercel (si es necesario)
 export default (req, res) => app(req, res);
